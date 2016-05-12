511bd4aef7b6a88101581a06a0383a62ee0765fd
NoobStudieux <emaurice@hotmail.fr>
2016-05-12 19:36:37 +0200

    modif des fichiers Model/module_de_base.php et Model/module_de_base_local.php : ajout d'un mot de passe pour la connexion MySql (vient detre modifié)

--- Model/module_de_base.php
+++ Model/module_de_base.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=Matchs;charset=utf8','root','');
+	$bdd = new PDO('mysql:host=localhost;dbname=Matchs;charset=utf8','root','toor');
 }
 catch(Exception $e)
 {
